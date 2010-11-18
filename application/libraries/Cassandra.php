14dacb876207e0214a1fbb81b95c6355ca9fa0e7
Ian Livingstone <ianl@cs.dal.ca>
2010-11-18 13:26:00 -0400

    Spacing and Indentation Fix
    I have fixed several spacing and indentation problems.

--- application/libraries/Cassandra.php
+++ application/libraries/Cassandra.php
@@ -1,78 +1,80 @@
 <?php
-//	Define the CodeIgniter Cassandra Constants
+//      Define the CodeIgniter Cassandra Constants
 define('CI_CASSANDRA_BASE',dirname(__FILE__)).'phpcassa/');
 define('CI_CASSANDRA_THRIFT',CI_CASSANDRA_BASE.'thrift/');
 
-//	Load the required files to use Thrift
+//      Load the required files to use Thrift
 require_once CI_CASSANDRA_THRIFT.'packages/cassandra/Cassandra.php';
 require_once CI_CASSANDRA_THRIFT.'transport/TSocket.php';
 require_once CI_CASSANDRA_THRIFT.'protocol/TBinaryProtocol.php';
 require_once CI_CASSANDRA_THRIFT.'transport/IFramedTransport.php';
 require_once CI_CASSANDRA_THRIFT.'transport/IBufferedTransport.php';
 
-//	Load the PHPCassa files
+//      Load the PHPCassa files
 include_once(CI_CASSANDRA_BASE.'phpcassa.php');
 include_once(CI_CASSANDRA_BASE.'uuid.php');
 
 class Cassandra {
-	
-	/*
-	*	CodeIgniter PHPCassa Implementation
-	*	Implemented by Ian Livingstone (http://www.ianlivingstone.ca)
-	*
-	*	More details here
-	*/
-	
-	//	Define the class data members
-	private $CI;
-	private $_config = array();
-	private $_columns = array();
-	private $_nodes = array();
+        
+        /*
+        *       CodeIgniter PHPCassa Implementation
+        *       Implemented by Ian Livingstone (http://www.ianlivingstone.ca)
+        *
+        *       More details here
+        */
+        
+        //      Define the class data members
+        private $CI;
+        private $_config = array();
+        private $_columns = array();
+        private $_nodes = array();
 
-	public function __construct () {
-		
-		/*
-		*	Class Constructor
-		*/
+        public function __construct () {
+                
+                /*
+                *       Class Constructor
+                */
 
-		//	Get reference to the CodeIgniter superobject
-		$this->CI =& get_instance();
+                //      Get reference to the CodeIgniter superobject
+                $this->CI =& get_instance();
 
-		//	Load the required codeigniter classes
-		//	None here for now
+                //      Load the required codeigniter classes
+                //      None here for now
 
-		//	Initialize the class
-		$this->_initialize();
-	}
-
-	private function _initialize () {
-		
-		/*
-		*	Initialize Cassandra Class
-		*	
-		*	Loads values from the cassandra.php config file into
-		*	the _nodes and _columns array. Constructs the required
-		*	classes to create column => ColumnObject mapping
-		*	and adds the nodes to the CassandraConn static obj	
-		*/
-
-        $this->CI->config->load('cassandra', true);
-        $this->_config = $this->CI->config->item('cassandra');
-
-        foreach ($this->_config['nodes'] as $node) {
-            CassandraConn::add_node($node['hostname'], $node['port']); 
+                //      Initialize the class
+                $this->_initialize();
         }
 
-        foreach ($this->_config['columns'] as $column) {
-            $this->_columns[$column['name']] = new CassandraCF($column['keyspace'],
-                                                               $column['name'],
-                                                               $column['supercolumn'],
-                                                               $column['column_type'],
-                                                               $column['subcolumn_type'],
-                                                               $column['read_consistency_level'],
-                                                               $column['write_consistency_level']);
+        private function _initialize () {
+                
+            /*
+            *       Initialize Cassandra Class
+            *       
+            *       Loads values from the cassandra.php config file into
+            *       the _nodes and _columns array. Constructs the required
+            *       classes to create column => ColumnObject mapping
+            *       and adds the nodes to the CassandraConn static obj      
+            */
+
+            $this->CI->config->load('cassandra', true);
+            $this->_config = $this->CI->config->item('cassandra');
+
+            foreach ($this->_config['nodes'] as $node) {
+                CassandraConn::add_node($node['hostname'], $node['port']); 
+            }
+
+            foreach ($this->_config['columns'] as $column) {
+                $this->_columns[$column['name']] = new CassandraCF(
+                    $column['keyspace'],
+                    $column['name'],
+                    $column['supercolumn'],
+                    $column['column_type'],
+                    $column['subcolumn_type'],
+                    $column['read_consistency_level'],
+                    $column['write_consistency_level']
+                );
+            }
         }
-	}
     
     public function get ($col, $key, $super_column = NULL, $slice_start="", $slice_finish="", $column_reversed=False, $column_count=100) {
         
